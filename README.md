1da162f0f4e21a707218ee5704b16248facb9914
spline <spline@spline.com>
2012-11-17 21:39:16 -0500

    Redid entire plugin since original source broke/changed. However, new source has a nice and clean XML api.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 Supybot-Bible
 =============
 
-Bible plugin for SupybotFriend of mine wanted a working Bible plugin. I didn't know much so he gave me some basics and I
-fixed up the scraping. There are a few sites out there offering verses from one or two books (versions?)
-but this goes from a site that has quite a few. 
+Plugin for a friend to display Bible passages. Uses a simple XML api. Currently only displays KJV.
--- plugin.py
+++ plugin.py
@@ -1,7 +1,10 @@
-import re
-import urllib
+
 import urllib2
-import json
+import urllib
+try:
+    import xml.etree.cElementTree as ElementTree
+except ImportError:
+    import xml.etree.ElementTree as ElementTree
 
 import supybot.utils as utils
 from supybot.commands import *
@@ -9,83 +12,56 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
-# http://www.4-14.org.uk/xml-bible-web-service-api
-# def bible(book, chapter, verse):
-#    url = "http://api.preachingcentral.com/bible.php?passage=%s%s:%s&version=asv" % (book, chapter, verse)
-#    parsed = lxml.html.fromstring(urllib2.urlopen(url).read())
-#    b = parsed.find('range').find('item').find('bookname').text_content()
-#    c = parsed.find('range').find('item').find('chapter').text_content()
-#    v = parsed.find('range').find('item').find('verse').text_content() 
-#    t = parsed.find('range').find('item').find('text').text_content() 
-#    return "<{C4}{B}%s %s:%s{}: {B}%s{}>" % (b, c, v, t)
 
-class Bible( callbacks.Plugin ):
+
+class Bible(callbacks.Plugin):
     threaded = True
 
-    def bible(self, irc, msg, args, translation, search):
-      """<translation> <search>
-      Returns text from specified Bible transation matching search parameters.
-      Ex: kjv Job 3:14
-      """
+    # http://www.4-14.org.uk/xml-bible-web-service-api
+    def bible(self, irc, msg, args, optpassage):
+        """<passage>
+        Returns text from specified Bible transation matching search parameters.
+        Ex: Job 3:14
+        """
 
-      translation = translation.upper()
-      url = 'http://mobile.biblegateway.com/passage/?search=' + urllib.quote( search ) + '&version=' + translation
+        url = 'http://api.preachingcentral.com/bible.php?passage=' + urllib.quote(optpassage) + '&version=kjv'
 
-      # test the request/connection
-      try: 
-        req = urllib2.Request(url)
-        stream = urllib2.urlopen(req)
-        html = stream.read()
-      except urllib2.HTTPError, err:
-        irc.reply("Failed to load request")
-        self.log.warning("Failed to load request: %s" % err.code)
+        try: 
+            request = urllib2.Request(url, headers={"Accept" : "application/xml"})
+            u = urllib2.urlopen(request)
+        except:
+            irc.reply("Failed to load url: %s" % url)
+            return
+        
+        # now try to process XML.
+        try:
+            tree = ElementTree.parse(u)
+            document = tree.getroot()
+        except:
+            irc.reply("Failed to parse XML. Check logs.")
+            return
+        
+        # first check for when syntax is broke. They don't give a proper error message.
+        # Error return: ParseError: mismatched tag: line 1909, column 2
+        if document.find('range/result') is None:
+            irc.reply("ERROR: Failed to load/parse the verse or page. Check your syntax. ")
+            return
+        
+        # if you do give an invalid passage, it will spit out an error in XML. 
+        if document.find('range/error') is not None:
+            irc.reply("ERROR: '{0}' when searching for: {1}".format(document.find('range/error').text, optpassage))
+            return
+        
+        for node in document.findall('range/item'):
+            bookname = node.find('bookname')
+            chapter = node.find('chapter')
+            verse = node.find('verse') 
+            text = node.find('text')
+            irc.reply("{0} {1}:{2} :: {3}".format(ircutils.bold(bookname.text), ircutils.bold(chapter.text), ircutils.bold(verse.text), text.text))
+            
+    bible = wrap(bible, [('text')])
 
-      # test for content
-      _biblegatewayre = re.compile( r'div class="result-text-style-normal text-html ">\s(.*?)</div>', re.S | re.I)
-      m = _biblegatewayre.search( html )
-
-      if m:
-        title = re.search(r'.*meta.*title.*content="(.*?)"', html).group(1)
-        title = re.sub(r'Bible Gateway passage: ', '', title)
-
-        #verse = re.search(r'(.*?)-', title).group(1)
-        #fullversion = re.search(r'- (.*?)', title).group(1)
-        #self.log.info(fullversion)
-        #irc.reply(search)
-        #irc.reply(translation)
-
-        # description as well
-        description = re.search(r'.*meta.*description.*content="(.*?)"', html).group(1)
-
-        irc.reply(title)
-        irc.reply(description)
-      
-      elif '>0 Results' in html or 'No results found' in html:
-          irc.reply( 'No results found for %s.' % search )
-      else:
-          irc.error( 'Source page has changed formatting.  Plugin update required.' )
-    bible = wrap( bible, [ 'something', 'text' ] )
-
-    def votd(self, irc, msg, args):
-      """
-      Returns the verse of the day from biblegateway.com
-      """
-
-      url = 'http://mobile.biblegateway.com/votd/get/?format=json&version=KJV'
-      response = urllib.urlopen(url)
-      json_response = response.read()
-      response_obj = json.loads(json_response)
-
-      text = response_obj['votd']['text']
-      text = re.sub('&[rl]dquo;', '"', text)
-      display_ref = response_obj['votd']['display_ref']
-      version_id = response_obj['votd']['version_id']
-
-      output = text + " (" + ircutils.bold(ircutils.underline(display_ref)) + " (" + version_id + "))"
-
-      irc.reply(output)
-
-    votd = wrap(votd)
+ 
 
 
 Class = Bible
