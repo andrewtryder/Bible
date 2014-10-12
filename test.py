dd5bba4526db534eec8e79faea41dafa679bde46
spline <reticulatingspline@github.com>
2014-10-12 08:04:12 -0400

    Fix tests.

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class BibleTestCase(PluginTestCase):
     
     def testBible(self):
         self.assertRegexp('bible --version kjv Job 3:14', 'With kings and counsellers of the earth, which built desolate places for themselves')
-        self.assertRegexp('bible --version kjv web 3:15', 'or with princes who had gold, who filled their houses with silver')
+        self.assertRegexp('bible --version web Job 3:15', 'or with princes who had gold, who filled their houses with silver')
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
