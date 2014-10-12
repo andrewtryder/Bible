357d9f5c7e3e848b8ba290a3c797aa9dcbf4752b
spline <reticulatingspline@github.com>
2014-10-12 07:58:25 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ from supybot.test import *
 
 class BibleTestCase(PluginTestCase):
     plugins = ('Bible',)
+    
+    def testBible(self):
+        self.assertRegexp('bible --version kjv Job 3:14', 'With kings and counsellers of the earth, which built desolate places for themselves')
+        self.assertRegexp('bible --version kjv web 3:15', 'or with princes who had gold, who filled their houses with silver')
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
